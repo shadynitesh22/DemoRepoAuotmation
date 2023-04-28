567ecba2062d6ffc1612ab6505301e2f525d24c6
shadynitesh22 <nitesh.poudel@hotmail.com>
2023-04-28 10:41:26 +0545

    Testing Pipeline-- Second dump

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,5 +10,6 @@ done
 
 
 Xvfb :99 -screen 0 1920x1080x24 &
+# java -cp "/app/src/main/resources:/app/target/classes:/app/target/test-classes:/app/lib/*" org.testng.TestNG testng.xml
 
 mvn test -Dsurefire.suiteXmlFiles=testng.xml
